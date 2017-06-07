de55024089e6d6f9b5d562b7e46e096132bbf240
Waffel183 <koensanderse2000@gmail.com>
2017-06-07 16:06:56 +0200

    restructure
    added. images in file

--- Preloader.js
+++ Preloader.js
@@ -9,6 +9,9 @@ Game.Preloader.prototype = {
       this.load.image('win','Images/Menu/win.png');
       this.load.image('loading','Images/Menu/loading.png');
 
+      game.load.spritesheet('dead', 'Images/Sprites/deadSpritesheet.png',50,50);
+      game.load.spritesheet('shoot', 'Images/Sprites/goodfire.png',50,50);
+
       this.load.image('background','Images/Background/bg.png');
 
       game.load.image('diamond', 'Images/Sprites/diamond.png');
